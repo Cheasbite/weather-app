0c72a4a3cd76170620802f6f02cf6578e36d6258
Cheasese <cheesesep@gmail.com>
2026-02-13 19:14:07 +0700

    v1.0.1

--- weather-app/src/app/api/weather/route.ts
+++ weather-app/src/app/api/weather/route.ts
@@ -1,19 +1,48 @@
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
-    const Psearch = request.nextUrl.searchParams
-    const city = Psearch.get('city')
-    const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY
-    const GetGeoLocation = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`, {cache: `no-store`})
-    if (!GetGeoLocation.ok && city !== null) {
-        return Response.json({error: "Failed to fetch"}, {status: 404});
+const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY
+const no_cache = `no-store`
+const temperature_degree_type = "metric"
+
+async function geoLocation(city: string) {
+    const GeoLocation_request = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`, {cache: no_cache})
+
+    if (!GeoLocation_request.ok) {
+        return [null, null]
     }
-    const JSONGeoLocation = await GetGeoLocation.json()
+    const geoLocation_data = await GeoLocation_request.json()
+    const lat: number = geoLocation_data[0].lat
+    const lon: number = geoLocation_data[0].lon
 
-    const lat = JSONGeoLocation[0].lat
-    const lon = JSONGeoLocation[0].lon
-    const req = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
-    const data = await req.json()
+    return [lat, lon]
+}
 
-    return Response.json(data)
+async function cityLocation(lattitude: number | null, longtitude: number | null) {
+    if (lattitude === null && longtitude === null) {
+        return null
+    }
+    const weather_request = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&lon=${longtitude}&appid=${apiKey}&units=${temperature_degree_type}`)
+    const weather_data = await weather_request.json()
+
+    return weather_data
+}
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const user_city = searchParams.get('city')
+    if (user_city === null) {
+        return Response.json({error: "null", status: 404})
+    }
+
+    const [lat, lon] = await geoLocation(user_city)
+    if (lat === null && lon === null) {
+        return Response.json({error: "null", status: 404})
+    }
+
+    const weather_data = await cityLocation(lat, lon)
+    if (weather_data === null) {
+        return Response.json({error: "null", status: 404})
+    }
+
+    return Response.json(weather_data)
 }
--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -1,46 +1,78 @@
 "use client"
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
+
+const api_path = "/api/weather?"
+let input_placeholder = "Enter a City (Example: Phnom Penh)"
+
+function response() {
+  const [weather, setWeather] = useState(null)
+  const [city, setCity] = useState('')
+  const [userinput, setUserinput] = useState('')
+
+  const fetch_weather_data = async (e: ChangeEvent) => {
+    e.preventDefault();
+    if (userinput === "") {
+      return [fetch_weather_data, null, null, userinput, setUserinput]
+    }
+
+    const weather_response = await fetch(`${api_path}city=${userinput}`)
+    if (!weather_response.ok) {
+      alert("Error! " + userinput + " is invalid!")
+      return [fetch_weather_data, null, null, userinput, setUserinput]
+    }
+
+    const weather_data = await weather_response.json()
+    setWeather(weather_data)
+    setCity(userinput)
+  }
+
+  return [fetch_weather_data, weather, city, userinput, setUserinput]
+}
+
+function weatherDisplay(weather_array: any) {
+  if (weather_array === null) {
+    return (null);
+  }
+
+  return (
+    <section id="section">
+      <div id="0">
+        <h2>
+          <span className="m-25 text-violet-400/80">Result: <br /></span>
+          Location: {weather_array.name} <br />
+          Temperature: {weather_array.main.temp}°C <br />
+          Humidity: {weather_array.main.humidity} <br />
+          Description: {weather_array.weather[0].description}
+        </h2>
+      </div>
+    </section>
+  );
+}
+
+function cityInput(Handler: any, userinput: any, setUserinput: Function) {
+  return (
+    <form onSubmit={Handler}>
+      <input placeholder={input_placeholder} type="text" size={80} 
+      value={userinput}
+      onChange={(e) => setUserinput(e.target.value)} />
+    </form>
+  );
+}
 
 export default function Home() {
-  const [weather, setweather] = useState(null)
-  const [city, setCity] = useState('')
-  const [userinput, Setuserinput] = useState('')
 
-  const func = async (e: any) => {
-    e.preventDefault();
-    try {
-      const req = await fetch(`/api/weather?city=${userinput}`)
-      const data = await req.json();
-      setweather(data)
-      setCity(userinput)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const [Handler, weather, _, userinput, setUserinput] = response()
+
   return (
     <div>
       <main>
         <h1 id="init">Welcome to my<span id="w_dec"><strong>Weather Webpage</strong></span>!</h1>
         <p>Enter a <span id="City">City Name</span> and press <span id="Enter">Enter</span> to begin.</p>
-        <form onSubmit={func}>
-          <input placeholder="Enter a City (Example: Phnom Penh)" type="text" size={80} 
-          value={userinput}
-          onChange={(e) => Setuserinput(e.target.value)} />
-        </form>
-        {
-          weather && weather.main &&
-          <section id="section">
-            <div id="0">
-              <h2>
-                <span className="m-25 text-violet-400/80">Result: <br /></span>
-                Location: {weather.name} <br />
-                Temperature: {weather.main.temp}°C <br />
-                Humidity: {weather.main.humidity} <br />
-                Description: {weather.weather[0].description}
-              </h2>
-            </div>
-          </section>
-        }
+
+        {cityInput(Handler, userinput, setUserinput)}
+
+        {weatherDisplay(weather)}
+
         <footer>
           <p>&copy; My Weather Wabpage.</p>
         </footer>
